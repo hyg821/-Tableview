7444a917142ab600d2fa6826a4e36adfcc304fc5
hyg <934533623@qq.com>
2016-05-16 10:54:51 +0800

    Initial Commit

---
+++ 全面知识点Tableview/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  全面知识点Tableview
+//
+//  Created by 侯英格 on 16/5/16.
+//  Copyright © 2016年 侯英格. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 全面知识点Tableview/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  全面知识点Tableview
+//
+//  Created by 侯英格 on 16/5/16.
+//  Copyright © 2016年 侯英格. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
